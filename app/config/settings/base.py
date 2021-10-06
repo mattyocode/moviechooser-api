045a530625cc4129b7104518c68fbe79e84027c1
Matt <63904102+mattyocode@users.noreply.github.com>
2021-10-06 10:29:20 +0100

    just env variables outside commands

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -16,20 +16,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-print("ALLOWED_HOSTS >>>> ", ALLOWED_HOSTS)
-
-print("DEBUG in base >> ", DEBUG)
 
 if not DEBUG:
     SECURE_HSTS_SECONDS = 3600
