fd82d485a5c8a175cd7fcaefb0427c2cdebdd670
Matt <63904102+mattyocode@users.noreply.github.com>
2021-10-06 10:24:03 +0100

    just env variables outside commands

--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -2,23 +2,6 @@ from .base import *
 
 print("PROD SETTINGS RUN")
 
-DEBUG = os.environ.get("DEBUG", 0)
-
-print("DEBUG in prod >> ", DEBUG)
-
-if not DEBUG:
-    SECURE_HSTS_SECONDS = 3600
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    X_FRAME_OPTIONS = "DENY"
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-    SECURE_REFERRER_POLICY = "same-origin"
-
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 30,
