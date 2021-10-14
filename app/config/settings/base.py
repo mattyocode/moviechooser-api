85398c513e545d0d66dd85d7e58fc0ce6fb4ec20
Matt <63904102+mattyocode@users.noreply.github.com>
2021-10-14 21:53:36 +0100

    add config to allow CORS

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -37,6 +37,26 @@ if not DEBUG:
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
     SECURE_REFERRER_POLICY = "same-origin"
 
+
+CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = ["*"]
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1",
+#     "localhost",
+#     "http://0.0.0.0",
+#     "moviechooser.co.uk",
+#     "www.moviechooser.co.uk"
+# ]
+
+# CORS_ORIGIN_WHITELIST = [
+#     "http://127.0.0.1",
+#     "http://localhost",
+#     "http://0.0.0.0",
+#     "moviechooser.co.uk",
+#     "www.moviechooser.co.uk"
+# ]
 # Application definition
 
 INSTALLED_APPS = [
@@ -49,12 +69,14 @@ INSTALLED_APPS = [
     "rest_framework",
     "movies",
     "accounts",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
