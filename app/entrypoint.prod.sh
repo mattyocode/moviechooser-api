09f5e6c0635d89eccd10696c95f93f0702dfeb56
Matt <63904102+mattyocode@users.noreply.github.com>
2021-10-27 19:29:44 +0100

    remove commands from docker-compose.staging and .prod

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -12,6 +12,7 @@ then
 fi
 
 mkdir /logs
+python manage.py migrate
 gunicorn config.wsgi:application --bind 0.0.0.0:8000 --forwarded-allow-ips="18.168.29.188,172.26.14.23" \
 --workers 3 --capture-output --access-logfile /logs/gunicorn-access.log
 
