507af28bb1149756fbeb7f1d18af6d67a54350cf
Matt <63904102+mattyocode@users.noreply.github.com>
2021-11-08 12:16:19 +0000

    fix linting

--- app/accounts/tests/conftest.py
+++ app/accounts/tests/conftest.py
@@ -1,30 +1,29 @@
 import pytest
-from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
 from rest_framework_simplejwt.tokens import RefreshToken
 
-User = get_user_model()
+from accounts.models import CustomUser
 
 
 @pytest.fixture
 def auth_user_client():
-    user = User.objects.create_user(
+    user = CustomUser.objects.create_user(
         username="fixtureguy", email="fixture@user.com", password="testpw"
     )
     client = APIClient()
     refresh = RefreshToken.for_user(user)
     client.credentials(HTTP_AUTHORIZATION=f"JWT {refresh.access_token}")
-
+    print("conftest >>>", refresh)
     return client
 
 
 @pytest.fixture
 def auth_superuser_client():
-    user = User.objects.create_superuser(
+    user = CustomUser.objects.create_superuser(
         username="admin1", email="admin@user.com", password="admintestpw"
     )
     client = APIClient()
     refresh = RefreshToken.for_user(user)
     client.credentials(HTTP_AUTHORIZATION=f"JWT {refresh.access_token}")
-
+    print("conftest >>>", refresh)
     return client
