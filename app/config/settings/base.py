41d5dccad0559266296b2510625e7339c42cf1ba
Matt <63904102+mattyocode@users.noreply.github.com>
2021-11-11 23:10:44 +0000

    update settings

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -153,7 +153,7 @@ REST_FRAMEWORK = {
 SECRET_SIGNING_KEY = os.environ.get("SECRET_KEY")
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": False,
