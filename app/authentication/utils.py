eaca141eb3338f5f9bfa1f98ae0d1cd2bb9b9285
Matt <63904102+mattyocode@users.noreply.github.com>
2021-11-25 19:56:27 +0000

    added exception around send_mail

--- app/authentication/utils.py
+++ app/authentication/utils.py
@@ -4,6 +4,7 @@ import requests
 
 recaptcha_secret_key = os.environ.get("RECAPTCHA_SECRET_KEY")
 
+
 def recaptcha_submit(token):
     response = requests.post(
         f"https://www.google.com/recaptcha/api/siteverify?secret={recaptcha_secret_key}&response={token}",
