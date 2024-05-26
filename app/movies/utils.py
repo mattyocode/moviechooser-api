c198a9c60a92fe949ca80b3689df4c91ad1580cd
Matt <63904102+mattyocode@users.noreply.github.com>
2024-05-26 12:25:56 +0100

    Update request headers

--- app/movies/utils.py
+++ app/movies/utils.py
@@ -150,7 +150,10 @@ class OMDBFetch:
 
 def get_imdbids_from_webpage(url):
     # Make the HTTP request
-    response = requests.get(url)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers)
 
     # Check if the request was successful
     if response.status_code == 200:
