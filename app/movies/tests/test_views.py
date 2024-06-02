4547c00f5adc8e0f7aa4b9fc07e5715d880448fe
Matt <63904102+mattyocode@users.noreply.github.com>
2024-06-02 13:05:16 +0100

    print debug status in test

--- app/movies/tests/test_views.py
+++ app/movies/tests/test_views.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import os
 from unittest import mock
 
 import pytest
@@ -382,6 +383,11 @@ def test_get_genre_queryset_filtered_by_number_of_movies_most_entries_first(clie
     MovieWithGenreFactory.create(title="Funny 2 Tests", genre=["comedy"], runtime=150)
     MovieWithGenreFactory.create(title="Funny 3 Tests", genre=["comedy"], runtime=90)
     MovieWithGenreFactory.create(title="Scary 2 Tests", genre=["horror"], runtime=120)
+
+    DEBUG = int(os.environ.get("DEBUG", default=0))
+    print("DEBUG with default", DEBUG)
+    DEBUG = int(os.environ.get("DEBUG"))
+    print("DEBUG without default", DEBUG)
 
     resp = client.get("/api/genres/")
 
