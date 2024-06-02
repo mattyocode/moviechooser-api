75b4427e4614fe54e06d34a711b746090e30fafd
Matt <63904102+mattyocode@users.noreply.github.com>
2024-06-02 12:49:55 +0100

    Update entrypoint

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -11,6 +11,21 @@ then
     echo "PostgreSQL started"
 fi
 
+mkdir /log
+mkdir /logs
+# # List of files to create if they don't exist
+# files=("/log/debug.log" "/log/error.log" "/log/info.log")
+
+# # Loop through the list and create each file if it doesn't exist
+# for file in "${files[@]}"; do
+#   if [[ ! -f "$file" ]]; then
+#     touch "$file"
+#     echo "Created $file"
+#   else
+#     echo "$file already exists"
+#   fi
+# done
+
 python manage.py migrate
 gunicorn config.wsgi:application --bind 0.0.0.0:8000 --forwarded-allow-ips="18.168.29.188,172.26.14.23" \
 --workers 4 --threads 4 --capture-output --access-logfile /logs/gunicorn-access.log --timeout 300
